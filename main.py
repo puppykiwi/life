a6a6de51cb4b839c12bd7f2cdd090ab56912dfc6
puppykiwi <johnraymwendwa04@gmail.com>
2023-10-24 12:09:16 +0300

    gold

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import random
 pygame.init()
 
 
-FPS = 69
+FPS = 120
 TILE_SIZE = 20
+GREY = (0,0,0)
 BLACK = (0,0,0)
-GREY = (128,128,128)
 YELLOW = (255,255,0)
 WIDTH, HEIGHT = (800,800)
 GRID_WIDTH = WIDTH // TILE_SIZE
